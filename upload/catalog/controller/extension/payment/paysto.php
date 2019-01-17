f5a3e6a8d7ba0b0b658cc86115147870cad6d5f0
Alexey Agafonov <awa77@mail.ru>
2019-01-17 14:32:41 +0300

    Paysto add hash methods

--- upload/catalog/controller/extension/payment/paysto.php
+++ upload/catalog/controller/extension/payment/paysto.php
@@ -10,7 +10,7 @@ class ControllerExtensionPaymentPaymaster extends Controller
     const STATUS_TAX_OFF = 'N';
     const MAX_POS_IN_CHECK = 100;
     const BEGIN_POS_IN_CHECK = 0;
-
+    
     /**
      * Получение формы оплаты
      * @return mixed
@@ -20,7 +20,7 @@ class ControllerExtensionPaymentPaymaster extends Controller
         $data['button_confirm'] = $this->language->get('button_confirm');
         $data['button_back'] = $this->language->get('button_back');
         $data['action'] = 'https://paysto.ru/Payment/Init';
-
+        
         $this->load->language('extension/payment/paysto');
         $this->load->model('account/order');
         $order_totals = $this->model_account_order->getOrderTotals($this->session->data['order_id']);
@@ -30,7 +30,7 @@ class ControllerExtensionPaymentPaymaster extends Controller
             'low_order_fee',
             'coupon'
         ];
-
+        
         //service
         foreach ($order_totals as $service) {
             if (in_array($service['code'], $services) && ($service['value'] > 0)) {
@@ -42,9 +42,9 @@ class ControllerExtensionPaymentPaymaster extends Controller
                 ];
             }
         }
-
+        
         $order_products = $this->model_account_order->getOrderProducts($this->session->data['order_id']);
-
+        
         //product
         if ($order_products) {
             foreach ($order_products as $order_product) {
@@ -56,35 +56,30 @@ class ControllerExtensionPaymentPaymaster extends Controller
                 ];
             }
         }
-
+        
         if (count($data['order_check']) > self::MAX_POS_IN_CHECK) {
             $data['error_warning'] = $this->language->get('error_max_pos');
         }
-
+        
         $data['pos'] = self::BEGIN_POS_IN_CHECK;
         $this->load->model('checkout/order');
         $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
+        $amount = number_format($order_info['total'], 2, ".", "");
+        $currency = strtoupper($order_info['currency_code']);
+        $order_id = $this->session->data['order_id'];
         $now = time();
-
+        
         $data['x_login'] = $this->config->get('paysto_x_login');
         $data['x_email'] = $order_info['email'];
-        $data['x_fp_sequence'] = $this->session->data['order_id'];
-        $data['x_invoice_num'] = $this->session->data['order_id'];
-        $data['x_amount'] = number_format($order_info['total'], 2, ".", "");
-        $data['x_currency_code'] = strtoupper($order_info['currency_code']);
+        $data['x_fp_sequence'] = $order_id;
+        $data['x_invoice_num'] = $order_id;
+        $data['x_amount'] = $amount;
+        $data['x_currency_code'] = $currency;
         $data['x_fp_timestamp'] = $now;
-
-        // Адаптер для последующего получения подписи
-        $request = [
-            'LMI_MERCHANT_ID' => $data['x_login'],
-            'LMI_PAYMENT_NO' => $data['order_id'],
-            'LMI_PAYMENT_AMOUNT' => $data['amount'],
-            'LMI_CURRENCY' => strtoupper($data['lmi_currency'])
-        ];
-
-        $data['sign'] = $this->_getSign($request);
-        $data['description'] = $this->language->get('text_order') . ' ' . $data['order_id'];
-
+        $data['x_description'] = $this->language->get('text_order') . ' ' . $order_id;
+        $data['x_fp_hash'] = $this->get_x_fp_hash($this->config->get('paysto_x_login'), $order_id,
+            $now, $amount, $currency);
+        
         return $this->load->view('extension/payment/paysto', $data);
     }
     
@@ -101,9 +96,9 @@ class ControllerExtensionPaymentPaymaster extends Controller
      */
     private function get_x_fp_hash($x_login, $x_fp_sequence, $x_fp_timestamp, $x_amount, $x_currency_code)
     {
-        $arr = array($x_login, $x_fp_sequence, $x_fp_timestamp, $x_amount, $x_currency_code);
+        $arr = [$x_login, $x_fp_sequence, $x_fp_timestamp, $x_amount, $x_currency_code];
         $str = implode('^', $arr);
-        return hash_hmac('md5', $str, $this->paysto_secret);
+        return hash_hmac('md5', $str, $this->config->get('paysto_secret_key'));
     }
     
     
@@ -117,7 +112,7 @@ class ControllerExtensionPaymentPaymaster extends Controller
      */
     private function get_x_MD5_Hash($x_login, $x_trans_id, $x_amount)
     {
-        return md5($this->paysto_secret . $x_login . $x_trans_id . $x_amount);
+        return md5($this->config->get('paysto_secret_key') . $x_login . $x_trans_id . $x_amount);
     }
     
     /**
@@ -141,7 +136,7 @@ class ControllerExtensionPaymentPaymaster extends Controller
                         'LMI_SHOPPINGCART.ITEMS[' . $check['pos'] . '].TAX' => $check['tax'],
                     ];
                 }
-
+                
                 $data = array_merge([
                     'LMI_MERCHANT_ID' => $data['x_login'],
                     'LMI_PAYMENT_AMOUNT' => $data['amount'],
@@ -151,17 +146,17 @@ class ControllerExtensionPaymentPaymaster extends Controller
                     'SIGN' => $data['sign'],
                 ], $order_check);
             }
-
+            
             $this->log->write('---------PAYSTO START LOG---------');
             $this->log->write('---Вызываемый метод: ' . $method . '---');
             $this->log->write('---Описание: ' . $text . '---');
             $this->log->write($data);
             $this->log->write('---------PAYSTO END LOG----------');
         }
-
+        
         return true;
     }
-
+    
     /**
      * Получение ставок налога
      * @param $product_id
@@ -172,44 +167,28 @@ class ControllerExtensionPaymentPaymaster extends Controller
         $this->load->model('catalog/product');
         $product_info = $this->model_catalog_product->getProduct($product_id);
         $tax_rule_id = 3;
-
+        
         foreach ($this->config->get('paysto_classes') as $i => $tax_rule) {
             if ($tax_rule['paysto_nalog'] == $product_info['tax_class_id']) {
                 $tax_rule_id = $tax_rule['paysto_tax_rule'];
             }
         }
-
+        
         $tax_rules = [
             [
                 'id' => 0,
-                'name' => 'vat18'
+                'name' => 'Without VAT'
             ],
             [
                 'id' => 1,
-                'name' => 'vat10'
-            ],
-            [
-                'id' => 2,
-                'name' => 'vat0'
-            ],
-            [
-                'id' => 3,
-                'name' => 'no_vat'
-            ],
-            [
-                'id' => 4,
-                'name' => 'vat118'
-            ],
-            [
-                'id' => 5,
-                'name' => 'vat110'
+                'name' => 'With VAT'
             ]
         ];
-
+        
         return $tax_rules[$tax_rule_id]['name'];
-
+        
     }
-
+    
     /**
      * Неоплачено выброс URL
      * @return bool
@@ -219,28 +198,28 @@ class ControllerExtensionPaymentPaymaster extends Controller
         $this->response->redirect($this->url->link('checkout/checkout', '', 'SSL'));
         return true;
     }
-
+    
     /**
      * Заказ оформлен и оплачен удачно
      * @return bool
      */
     public function success()
     {
-
+        
         $order_id = $this->request->post["LMI_PAYMENT_NO"];
         $this->load->model('checkout/order');
         $order_info = $this->model_checkout_order->getOrder($order_id);
-
+        
         if ((int)$order_info["order_status_id"] == (int)$this->config->get('paysto_order_status_id')) {
             $this->model_checkout_order->addOrderHistory($order_id, $this->config->get('paysto_order_status_id'),
                 'Paysto', true);
             $this->response->redirect($this->url->link('checkout/success', '', 'SSL'));
             return true;
         }
-
+        
         return $this->fail();
     }
-
+    
     /**
      * Основной CallBack для проверки подписи и смене статуса заказа
      */
@@ -249,13 +228,13 @@ class ControllerExtensionPaymentPaymaster extends Controller
         if (!isset($this->request->post)) {
             exit();
         }
-
+        
         $order_id = $this->request->post["LMI_PAYMENT_NO"];
         $this->load->model('checkout/order');
         $order_info = $this->model_checkout_order->getOrder($order_id);
         $amount = number_format($order_info['total'], 2, '.', '');
         $x_login = $this->config->get('paysto_x_login');
-
+        
         if (isset($this->request->post['LMI_PREREQUEST'])) {
             if ($this->request->post['LMI_MERCHANT_ID'] == $x_login && $this->request->post['LMI_PAYMENT_AMOUNT'] == $amount) {
                 echo 'YES';
@@ -276,8 +255,7 @@ class ControllerExtensionPaymentPaymaster extends Controller
                         try {
                             $this->model_checkout_order->addOrderHistory($order_id,
                                 $this->config->get('paysto_order_status_id'), 'Оплачено через Paysto');
-                        }
-                        catch (\Exception $exception) {
+                        } catch (\Exception $exception) {
                             $this->log->write($exception->getMessage());
                             exit();
                         }
@@ -287,8 +265,7 @@ class ControllerExtensionPaymentPaymaster extends Controller
                         try {
                             $this->model_checkout_order->addOrderHistory($order_id,
                                 $this->config->get('paysto_order_status_id'), 'Paysto', true);
-                        }
-                        catch (\Exception $exception) {
+                        } catch (\Exception $exception) {
                             $this->log->write($exception->getMessage());
                             exit();
                         }
@@ -300,9 +277,9 @@ class ControllerExtensionPaymentPaymaster extends Controller
             }
         }
     }
-
+    
     //  Вспомогалельные функции
-
+    
     /**
      * Получение HASH
      * @param $request
@@ -316,12 +293,12 @@ class ControllerExtensionPaymentPaymaster extends Controller
             . $request["LMI_SYS_PAYMENT_ID"] . ";" . $request["LMI_SYS_PAYMENT_DATE"] . ";"
             . $request["LMI_PAYMENT_AMOUNT"] . ";" . $request["LMI_CURRENCY"] . ";" . $request["LMI_PAID_AMOUNT"] . ";"
             . $request["LMI_PAID_CURRENCY"] . ";" . $request["LMI_PAYMENT_SYSTEM"] . ";"
-            . $request["LMI_SIM_MODE"] . ";" .$secret_key;
+            . $request["LMI_SIM_MODE"] . ";" . $secret_key;
         $hash = base64_encode(hash($hash_alg, $plain_string, true));
         return $hash;
     }
-
-
+    
+    
     /**
      * Получение подписи
      * @param $request
