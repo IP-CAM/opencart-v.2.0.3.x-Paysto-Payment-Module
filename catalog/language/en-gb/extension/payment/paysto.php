1f952413ec037c6b67710d020cd5c5c74b6a90f9
Alexey Agafonov <awa77@mail.ru>
2019-01-23 16:57:57 +0300

    Correct construction function argument name

--- catalog/language/en-gb/extension/payment/paysto.php
+++ catalog/language/en-gb/extension/payment/paysto.php
@@ -1,5 +1,5 @@
 <?php
-$_['text_title']    = 'Paysto.RU';
+$_['text_title']    = 'Paysto';
 $_['text_order']    = 'Order';
 $_['text_success']  = 'The order has been successfully received and approved.';
 $_['text_waiting']  = 'Order successfully received.';
